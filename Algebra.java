7957acb7dab6112d191634f88657c1252ea991dd
roei gutman <git config --global user.email david.german@post.runi.ac.il>
2025-11-19 14:08:57 +0200

    Completed assignment

--- Algebra.java
+++ Algebra.java
@@ -5,9 +5,9 @@
 
 public class Algebra {
 	public static void main(String args[]) {
-	    // Tests some of the operations
+	    
 	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
+		System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
  		System.out.println(times(3,4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
@@ -18,50 +18,115 @@ public class Algebra {
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
    		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
+   		System.out.println(sqrt(36));  
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int x = x1;
+		
+		for(int i = 0; i < x2; i++) {
+		x++;
+		}	
+		return x;
 	}
-
+              
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		
+		int x = x1;
+
+	    for( int i = 0; i < x2; i++) {        // 2 - 7
+		}  
+        x--;
+		
+		return x;
+
+
+
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		
+        
+        int sum = 0;
+		for( int i = 0; i < x2; i++ ) {
+
+       sum = plus(sum, x1);
+
+		}
+
+		return sum;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
-	}
+		
+        
+        int sum = 1;
+		for ( int i = 0; i < n; i++) {
 
+         sum = times(sum, x);
+		}
+		return sum;
+
+	}
+	  
+	
+	
+	
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+		
+
+		
+        int i = x1 ;                            
+		int count = 0;                                          
+		                                            
+							
+		if( x1 < x2) {
+           return 0;              
+		}
+        while ( i >= x2) {       
+			
+		i = minus(i, x2);      
+		    count++;
+		  }
+
+		
+		return count;
+	
+    }
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		
+		if( x1 < x2)
+             return x1;
+
+		int n = x1;
+
+		for( int i = 0; i >= x2; i ++ )
+		    i= minus(n, x2);
+			
+		
+		
+		
+		return n;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		
+
+		
+		
+		
+		
+		
+		return 0; 
 	}	  	  
 }
--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,76 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
-	}
+		
+       str1 = preProcess(str1);
+	   str2 = preProcess(str2);
+		
+		
+		
+		
+		if( str1.length() != str2.length()) {
+    
+	   	return false;
+	   }
+        for( int i = 0; i < str1.length(); i++) {
+          
+		char c = str1.charAt(i);
+        int index = str2.indexOf(c);
+		
+		
+		if ( index == -1 ) {
+	        return false;
+			
+		} else {  str2 = str2.substring(0, index) + str2.substring(index + 1);
+
+		  }
+		}
+	
+	   return true;
+     }
+	
+	
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		
+       String new1 = "";
+
+	   for ( int i=0; i < str.length(); i++ ) {
+        char c = str.charAt(i);
+         
+         if( (c >= 'a' && c <= 'z') ||  (c >= 'A' && c <= 'Z')) {
+
+			new1 += c;
+
+		 }
+		 }
+		return new1.toLowerCase();
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+
+   
+
+	String new2 = "";
+    int s = str.length();
+	for ( int i=0; i < s; i++) {
+
+		int index = (int)((Math.random() * str.length()));
+        
+		new2 += str.charAt(index);
+		
+		str = str.substring(0, index) + str.substring(index + 1);
+		}
+  
+	return new2;
+	
+	
 	}
+
 }
+
